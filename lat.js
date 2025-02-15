b51de1c8255e995daacc054aa5f045af30cbde4f
mateus <matheus2022y@gmail.com>
2025-02-15 15:02:58 -0300

    iniciado repositorio

---
+++ lat.js
@@ -0,0 +1,18 @@
+let menuitem = document.querySelectorAll('.item-menu')
+
+function sele(){
+    menuitem.forEach((item)=>
+    item.classList.remove('ativo')
+    )
+    this.classList.add('ativo')
+}
+menuitem.forEach((item)=>
+item.addEventListener('click',sele)
+)
+//fazer expandir
+
+let exp = document.querySelector('#btn-exp')
+let menuside = document.querySelector('.menu-lateral')
+exp.addEventListener('click',function(){
+    menuside.classList.toggle('expandir')
+})
